fe9993f182d1ff9c32490dc43a89ec29669b657f
YoungKrug <gregjohn456@gmail.com>
2019-11-14 09:13:27 -0500

    Fixed minor issues

---
+++ SeniorProject/Intermediate/Build/Win64/UE4/Inc/SeniorProject/SeniorProjectGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SENIORPROJECT_SeniorProjectGameModeBase_generated_h
+#error "SeniorProjectGameModeBase.generated.h already included, missing '#pragma once' in SeniorProjectGameModeBase.h"
+#endif
+#define SENIORPROJECT_SeniorProjectGameModeBase_generated_h
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_RPC_WRAPPERS
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASeniorProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASeniorProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASeniorProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/SeniorProject"), NO_API) \
+	DECLARE_SERIALIZER(ASeniorProjectGameModeBase)
+
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASeniorProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASeniorProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASeniorProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/SeniorProject"), NO_API) \
+	DECLARE_SERIALIZER(ASeniorProjectGameModeBase)
+
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASeniorProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASeniorProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASeniorProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASeniorProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASeniorProjectGameModeBase(ASeniorProjectGameModeBase&&); \
+	NO_API ASeniorProjectGameModeBase(const ASeniorProjectGameModeBase&); \
+public:
+
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASeniorProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASeniorProjectGameModeBase(ASeniorProjectGameModeBase&&); \
+	NO_API ASeniorProjectGameModeBase(const ASeniorProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASeniorProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASeniorProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASeniorProjectGameModeBase)
+
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_12_PROLOG
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_RPC_WRAPPERS \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_INCLASS \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SENIORPROJECT_API UClass* StaticClass<class ASeniorProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SeniorProject_Source_SeniorProject_SeniorProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
